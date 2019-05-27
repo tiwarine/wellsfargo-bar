57dfe975d2b75979b41344a056f635e715f258c6
jv45 <33951889+jv45@users.noreply.github.com>
2019-05-27 14:26:03 +0530

    Test notification

--- script.sh
+++ script.sh
@@ -12,3 +12,4 @@ echo "Machine:" `uname -a` >> test
 echo "user: $var1 || Environment: $env" >> test
 echo "[INFO] Git jenkins integration"
 echo "[INFO] Poll SCM Automatic Trigger"
+Echo "[INFO] Test notification"
